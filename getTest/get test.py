335e872d61043295e6d591fc382067efce0ab509
Merouane B <merouane@stamp.yt>
2021-10-13 16:02:07 +0200

    getTest is working

--- getTest/get test.py
+++ getTest/get test.py
@@ -31,28 +31,101 @@ from datetime import datetime
 
 
 # ------------------------------
+# SCRIPT FILE INFO
+# ------------------------------
+SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
+SCRIPT_NAME = os.path.basename(sys.argv[0])
+
+# ------------------------------
 # CONSTANT
 # ------------------------------
 INPUT_FILEPATH               = './input.csv'
 SLEEP_TIME_RETRY_REQUEST_SEC = 1
 NB_TRY_REQUEST               = 3
+NB_OF_OBJECT_LIMIT = 1
 
-pomme='pomme'
+BASE_URL_KEY = 'BASE_URL'
+API_KEY_KEY = 'API_KEY'
+
+# ------------------------------
+# UTILS
+# ------------------------------
+def sourceEnvLog(key):
+    return f'You need to source environment variables (no {key} found in OS env)'
+
+# ------------------------------
+# ENVIRONEMENT VARIABLE
+# ------------------------------
+def setEnvVar():
+    (base_url, api_key) = ('', '')
+    if BASE_URL_KEY in os.environ:
+        base_url = os.environ[BASE_URL_KEY]
+    else:
+        log.error(sourceEnvLog(BASE_URL_KEY))
+        sys.exit(1)
+
+    if API_KEY_KEY in os.environ:
+        api_key = os.environ[API_KEY_KEY]
+    else:
+        log.error(sourceEnvLog(API_KEY_KEY))
+        sys.exit(1)
+
+    return (base_url, api_key)
+
+# ------------------------------
+# API CALL
+# ------------------------------
+def getGifForQuery(log, base_url, api_key, query):
+#ASK: why .format
+
+  end_point = f'{base_url}gifs/search?api_key={api_key}&q={query}&limit={NB_OF_OBJECT_LIMIT}'
+
+#   headers                 = {}
+#   headers['Accept']       = 'application/json'
+#   headers['Content-Type'] = 'application/json'
+#   payload                 = {}
+
+  status_ok               = [200]
+
+  number_of_try_request = 0
+  status = 0
+  response = None    
+
+  while (status not in status_ok) and number_of_try_request <= NB_TRY_REQUEST:
+    try:    
+      response = requests.get(end_point)
+    except requests.exceptions.RequestException as e:
+      log.warning("Fail call API {} : {}".format(end_point, e))
+      time.sleep(SLEEP_TIME_RETRY_REQUEST_SEC)
+      continue
+    status = int(response.status_code)
+    if status not in status_ok:
+      log.warning('{} : [{}]{}'.format(query, response.status_code, response.text.rstrip()))
+      log.debug('{} : [{}]{} ({})'.format(query, response.status_code, response.text.rstrip(), response.headers))
+      time.sleep(SLEEP_TIME_RETRY_REQUEST_SEC)
+    if number_of_try_request >= NB_TRY_REQUEST:
+      log.error('Fail to get a 200 HTTP response on : {}, nb_retry={}'.format(end_point, NB_TRY_REQUEST))
+    number_of_try_request += 1
+
+  return response.json()
 
 
 def main():
+    (base_url, api_key) = setEnvVar()
 
-    if 'BASE_URL' in os.environ:
-        base_url = os.environ['BASE_URL']
-    else:
-        log.error('You need to source environment variables (no BASE_URL found in OS env)')
-        sys.exit(1)
+    with open(INPUT_FILEPATH, 'r') as input_file:
+        input_file_content = input_file.read().splitlines() 
+        result_file = os.path.join(SCRIPT_PATH, 'ouput_' + os.path.splitext(SCRIPT_NAME)[0] + '.csv')
 
-    if 'API_KEY' in os.environ:
-        api_key = os.environ['API_KEY']
-    else:
-        log.error('You need to source environment variables (no API_KEY found in OS env)')
-        sys.exit(1)
+        with open(result_file, 'w') as result_file_content:
+            for line in input_file_content:
+                if not line.startswith("#"):
+                    response = getGifForQuery(log, base_url, api_key, line)
+                    
+                    gifUrl = response['data'][0]['url']
+                    log.info(gifUrl)
+                    result_file_content.write(f'{gifUrl}\n')
 
+        time.sleep(SLEEP_TIME_RETRY_REQUEST_SEC) 
     
 main()
